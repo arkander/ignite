43ecc9051302bb513840ceb426a545f91635c8ef
Adauto <adautofabiano.silva@gmail.com>
2021-04-03 20:17:15 -0300

    setup das demais urls de carregamento de jogs

--- src/api.js
+++ src/api.js
@@ -13,13 +13,16 @@ const getCurrentDay = ()=>{
 const currentYear = new Date().getFullYear();
 const currentMonth = getCurrentMonth();
 const currentDay = getCurrentDay();
+
 const currentDate = `${currentYear}-${currentMonth}-${currentDay}`;
 const lastYear = `${currentYear - 1}-${currentMonth}-${currentDay}`;
 const nextYear = `${currentYear + 1}-${currentMonth}-${currentDay}`;
 
-
+//api url for games
 const popular_games = `games?dates=${lastYear},${currentDate}&ordering=-rating&page_size=10`;
+const upcoming_games = `games?dates=${currentDate},${nextYear}&ordering=-added&page_size=10`;
+const newGames = `games?dates=${lastYear},${currentDate}&ordering=-released&page_size=10`;
 
 export const popularGamesURL = ()=>`${base_url}${popular_games}`;
-
-
+export const upcomingGamesURL = ()=>`${base_url}${upcoming_games}`;
+export const newGamesURL = ()=>`${base_url}${newGames}`;
