92e0dbf3329b9eb6c15007bc84ee9f9b62c50aad
Adauto <adautofabiano.silva@gmail.com>
2021-04-03 18:24:50 -0300

    Criação de urls de acesso da api

---
+++ src/api.js
@@ -0,0 +1,25 @@
+const base_url = 'https://api.rawg.io/api/';
+
+const getCurrentMonth = ()=>{
+    const month = new Date().getMonth() + 1;
+    return month < 10 ? `0${month}` : month;
+}
+
+const getCurrentDay = ()=>{
+    const day = new Date().getDate() ;
+    return day < 10 ? `0${day}` : day;
+}
+
+const currentYear = new Date().getFullYear();
+const currentMonth = getCurrentMonth();
+const currentDay = getCurrentDay();
+const currentDate = `${currentYear}-${currentMonth}-${currentDay}`;
+const lastYear = `${currentYear - 1}-${currentMonth}-${currentDay}`;
+const nextYear = `${currentYear + 1}-${currentMonth}-${currentDay}`;
+
+
+const popular_games = `games?dates=${lastYear},${currentDate}&ordering=-rating&page_size=10`;
+
+export const popularGamesURL = ()=>`${base_url}${popular_games}`;
+
+
